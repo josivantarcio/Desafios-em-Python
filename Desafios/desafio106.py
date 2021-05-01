90d6c0cfc8d6c954fee8e376969f64f87640c992
Josevan Oliveira <josivantarcio@gmail.com>
2021-05-01 18:19:03 -0300

    Update desafio106.py
    exercicio 106 com comentarios

--- Desafios/desafio106.py
+++ Desafios/desafio106.py
@@ -1,16 +1,38 @@
 def ajuda(msn):
-    print('\033[1;34;43m')
+    """Funcao de apresentar o help em Python
+
+    Args:
+        msn (str): Faz chamada para a variavel fun do programa principal
+
+    Returns:
+        str: Retorno da funcao help do Python
+    """
+    print('\033[0;34;47m')
     return help(msn)
 
 def corCabecalho():
+    """Imprime o cabecalho colorido
+    """
     print('\033[0;33;45m')
     print('=' * 19)
-    print('AJUDA DO SISTEMA')
+    print('  AJUDA DO SISTEMA')
     print('=' * 19)
 
-
+def corRodape():
+    """Imprime o rodapé colorido
+    """
+    print('\033[0;30;46m')
+    print('=' * 19)
+    print('   FIM')
+    print('=' * 19)
+    
+    
 #main
-corCabecalho()
-fun = str(input('Digite o codigo: ')).lower().strip()
-
-desc = ajuda(fun)
+while True:
+    corCabecalho()
+    fun = str(input('Digite a ajuda: ')).strip()
+    if fun in 'fim':
+        break
+    else:
+        desc = ajuda(fun)
+corRodape()
